9e6b103a7f4db9a0ba4e1454686b27748af70f72
Mahdhi Rezvi <mahdhi_rezvi@yahoo.com>
2021-06-21 15:21:35 +0530

    Created basic graphql server

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
+const { buildSchema } = require('graphql');
 const app = express();
 
 app.use(express.json());
 
-app.get('/',(req, res, next)=>{
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery{
+            events: [String!]!
+        }
+
+        type RootMutation{
+            createEvent(name:String!):String
+        }
+
+        schema {
+            query: RootQuery
+            mutation:RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Sample Item 1', 'Sample Item 2']
+        },
+        createEvent: (args) => {
+            return args.name.toUpperCase();
+        }
+    },
+    graphiql: true
+}))
+
+app.get('/', (req, res, next) => {
     res.send('Hello World!');
 })
 
